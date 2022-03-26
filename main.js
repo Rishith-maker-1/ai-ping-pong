77ea20c21660a51f72ba320ecff7bb44ee3a3cef
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2022-03-26 16:47:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,10 +24,20 @@ var ball = {
 
 function setup() {
   var canvas = createCanvas(700, 600);
+  canvas.parent('canvas');
+  video=createCapture(VIDEO);
+  video.hide();
+  video.size(700,600);
+  posenet=ml5.poseNet(video,modelLoaded);
 }
 
+function modelLoaded(){
+  console.log("Model loaded");
+}
 
 function draw() {
+
+  image(video,0,0,700,600);
 
   background(0);
 
